0bf36dc0700ebdd9dba8658ec465abd7220bb4f1
Anitha U <anithaumuhire@gmail.com>
2019-10-16 19:00:10 +0200

    add tests

--- myinsta/tests.py
+++ myinsta/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 from .models import Comments,Profile,Image
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.image= Image()
+
+     # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))     

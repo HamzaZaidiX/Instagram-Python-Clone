2e8e64f3855fd60e90785f10605d01b3daf9e463
Anitha U <anithaumuhire@gmail.com>
2019-10-17 10:32:00 +0200

    add tests

--- myinsta/models.py
+++ myinsta/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Profile(models.Model):
     photo = models.ImageField(upload_to = 'photos',null=True)
     biography=models.TextField(max_length=60)
-     
+ 
     def __str__(self):
         return self.profile
 
@@ -17,6 +17,16 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save_image(self):
+        return self.save() 
+    
+    def update_image(self):
+        return self.update() 
+
+    def delete_image(self):
+        return self.delete() 
+
 
 class Comments(models.Model):
     comments=models.CharField(max_length=30) 
--- myinsta/tests.py
+++ myinsta/tests.py
@@ -6,8 +6,24 @@ class ImageTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.image= Image()
+        self.animal= Image()
 
      # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.image,Image))     
+        self.assertTrue(isinstance(self.animal,Image))  
+
+    def test_save_method(self):
+        self.animal.save_image()
+        images = Image.objects.all() 
+        self.assertTrue(len(images)>0)      
+    
+
+    def test_update_method(self):
+        self.animal.update_image()
+        images = Image.objects.filter(id=2).update(image='zebra') 
+        self.assertTrue(len(images)==0)   
+
+    def test_delete_method(self):
+        self.animal.delete_image()
+        images=Image.objects.filter(animal=zebra).delete() 
+        self.assertTrue(len(images)==0)     

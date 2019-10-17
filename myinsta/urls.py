8ccff5d8ed2882c7ceca2a62529f3dae082b8be9
Anitha U <anithaumuhire@gmail.com>
2019-10-17 19:05:15 +0200

    add url of index

--- myinsta/urls.py
+++ myinsta/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from django.contrib.auth import views 
 
 urlpatterns = [
-    url('^$',views.index,name='insta')
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^index/$',views.index,name='insta')
+    url(r'^accounts/', include('registration.backends.simple.urls')), 
 ]    
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

852aa80264908e089fca8f46bad16a9bf76db1ea
Anitha U <anithaumuhire@gmail.com>
2019-10-17 14:55:06 +0200

    update settings

--- myinsta/urls.py
+++ myinsta/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth import views 
 
 urlpatterns = [
+    url('^$',views.index,name='insta')
     url(r'^accounts/', include('registration.backends.simple.urls')),
-]
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+]    
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- myinsta/views.py
+++ myinsta/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index():
+    images=Image.objects.all()
+    return render(request,'index.html',{'myinsta':images})
